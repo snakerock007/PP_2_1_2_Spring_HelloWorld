bcbade39b4cb8a8e11310b7bcd4664adfe767b81
Bolotov Alexander <distalker1@gmail.com>
2022-01-18 08:42:56 +0300

    fix pom

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,21 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+
+        boolean comparison = bean == bean2;
+        System.out.println(comparison);
+
+        boolean comparison2 = cat == cat2;
+        System.out.println(comparison2);
+
+
+
         System.out.println(bean.getMessage());
+        System.out.println(cat.getMessage());
     }
 }
